8b6646159d8255b496977c5cac34a86b031183cf
Nykollas <nicolauarti09@gmail.com>
2019-10-10 08:03:07 -0300

    Adicionando dependência xmltodict

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Nykollas/correiosws",
     packages=["correiosws"],
+    install_requires=["xmltodict"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
